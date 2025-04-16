9bbcc2fc20737bef96e94c99098b7fbe0d1dd299
Nik Kuchler <nkuchler2000@gmail.com>
2025-04-16 10:24:32 +0200

    some changes

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Fxt8OHBZ18Zv1FDO6h2AF+9neD4pjBCRE2ZlkPL8XjY=\"\n}"
